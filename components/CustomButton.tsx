cdeb83310239e2881441cec739d2d87d7298e960
shuang <shuang.lixx@qq.com>
2023-06-28 21:13:28 +0800

    完成carCard组件

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { CustomButtonProps } from "../types";
-
-export const CustomButton:React.FC<CustomButtonProps> = ({title, containerStyles, handleClick, btnType}) => {
+import Image from "next/image";
+export const CustomButton:React.FC<CustomButtonProps> = ({title, containerStyles, handleClick, btnType,
+  textStyles, rightIcon,isDisabled
+}) => {
   return (
     <button
       disabled={false}
@@ -10,9 +12,19 @@ export const CustomButton:React.FC<CustomButtonProps> = ({title, containerStyles
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>
+      <span className={`flex-1 ${textStyles}`}>
         {title}
       </span>
+      {rightIcon && (
+        <div className=" relative w-6 h-6">
+          <Image 
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className=" object-contain"
+          />
+        </div>
+      )}
     </button>
   )
 }
---
+++ utils/index.ts
@@ -0,0 +1,29 @@
+
+export async function fetchCars(){
+  const headers = {
+    'X-RapidAPI-Key': 'aeceb7d62amsh54957300d70061bp1e7380jsn0dfe695ec2bc',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+  }
+  const response = await fetch(
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
+    {
+      headers
+    }
+  )
+  const result = await response.json()
+  return result;
+}
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50 // Base rental price per day in dollars
+  const mileageFactor = 0.1 // Additional rate per mile driven
+  const ageFactor = 0.05 // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor
+  const ageRate = (new Date().getFullYear() - year) * ageFactor
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
+
+  return rentalRatePerDay.toFixed(0)
+}
