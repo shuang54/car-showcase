5b79f0f79dd661c9a7246d53fd31cd14acbaa7e0
shuang <shuang.lixx@qq.com>
2023-07-01 22:04:44 +0800

    添加获取汽车图片api

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
+import { CarProps } from "@/types";
 
 export async function fetchCars(){
   const headers = {
@@ -5,7 +6,7 @@ export async function fetchCars(){
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
   }
   const response = await fetch(
-    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=a3',
     {
       headers
     }
@@ -27,3 +28,20 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
   return rentalRatePerDay.toFixed(0)
 }
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/getimage')
+  const { make, model, year } = car
+
+  url.searchParams.append(
+    'customer',
+    process.env.NEXT_PUBLIC_IMAGIN_API_KEY || 'hrjavascript-mastery'
+  )
+  url.searchParams.append('make', make)
+  url.searchParams.append('modelFamily', model.split(' ')[0])
+  url.searchParams.append('zoomType', 'fullscreen')
+  url.searchParams.append('modelYear', `${year}`)
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append('angle', `${angle}`)
+
+  return `${url}`
+} 
