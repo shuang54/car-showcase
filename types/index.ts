9306089da8cea3ea9a0ac8eade3b3f57174c8e92
shuang <shuang.lixx@qq.com>
2023-07-02 14:14:40 +0800

    customfilter

--- types/index.ts
+++ types/index.ts
@@ -39,3 +39,11 @@ export interface FilterProps {
 export interface HomeProps {
   searchParams: FilterProps
 }
+export interface OptionProps {
+  title: string
+  value: string
+}
+export interface CustomFilterProps {
+  title: string
+  options: OptionProps[]
+}
